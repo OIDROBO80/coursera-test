47b271d7b291a113923a21f14f7833347b6a05dc
Oscar Idrobo <OIDROBO@BANCODEBOGOTA.COM.CO>
2021-04-13 00:02:55 -0500

    solution module 2

---
+++ module2-solution/app.js
@@ -0,0 +1,72 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+    var listToBuy= this;
+    listToBuy.items= ShoppingListCheckOffService.getToBuy();
+    listToBuy.buy = function (itemIndex) {
+        ShoppingListCheckOffService.buy(itemIndex);
+    }
+    listToBuy.Empty=function(){
+        return listEmpty(listToBuy.items.length);
+    }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var listBought  = this;
+  listBought.items = ShoppingListCheckOffService.getbought();
+  listBought.Empty=function(){
+    return listEmpty(listBought.items.length);
+  }
+}
+function listEmpty(lengthList){
+    if(lengthList==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // items to buy
+  var toBuy = [{
+    name: 'rice',
+    quantity: 10
+  },{
+    name: 'onion',
+    quantity: 1
+  },{
+    name: 'cereal',
+    quantity: 9
+  }];
+  var lengthToBuy=toBuy.length;
+  var bought = [];
+  
+
+  service.buy = function (itemIndex) {
+    bought.push(toBuy[itemIndex]);
+    toBuy.splice(itemIndex, 1);
+  };
+  service.getToBuy= function () {
+    return toBuy;
+  };
+  service.LengthListToBuy= function () {
+    return lengthToBuy;
+  };
+  service.getbought= function () {
+    return bought;
+  };
+ 
+}
+    
+})();
+    
